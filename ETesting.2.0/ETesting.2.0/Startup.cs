5258a38ed96e535485eb3471b9c55d60dc39d51d
Xuân Khoa <nguyenxuankhoactk35@gmail.com>
2016-12-05 20:56:00 +0700

    Create Solution

---
+++ ETesting.2.0/ETesting.2.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ETesting._2._0.Startup))]
+namespace ETesting._2._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
